64ac332680f802c8795842e48eaccd5ef6223335
Mark Mann <mann.brinson@gmail.com>
2021-04-13 13:23:22 -0700

    details page completed

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.set('json spaces', 2)
 //GLOBAL PARAMETERS
 var api_key = "2e510746ca28d7041056c7e57108de4c"
 
+// GLOBAL FUNCTIONS
+function buildReq(url) {
+    //Construct a request object for each url
+    const req = axios.get(url);
+    return req
+}
+
 //////////// ROUTES ////////////
 //// HOME
 app.get('/apis/home/:entity', function(req, res) {
@@ -153,10 +160,202 @@ app.get('/apis/home/:entity', function(req, res) {
 })
 
 //// DETAILS PAGE
-// app.get('/apis/watch/:entity/:tmdb_id', function (req, res) {
-//     res.send("Hello world")
+app.get('/apis/watch/:entity/:tmdb_id', function (req, res) {
+
+    //Get query parameters
+    entity = req.params.entity
+    tmdb_id = req.params.tmdb_id
+
+    //Build requests
+    url_root = "https://api.themoviedb.org/3/"
+    url_detail = `${url_root}${entity}/${tmdb_id}?api_key=${api_key}`
+    url_video = `${url_root}${entity}/${tmdb_id}/videos?api_key=${api_key}`
+    url_credits = `${url_root}${entity}/${tmdb_id}/credits?api_key=${api_key}`
+    url_reviews = `${url_root}${entity}/${tmdb_id}/reviews?api_key=${api_key}`
+    url_recommended = `${url_root}${entity}/${tmdb_id}/recommendations?api_key=${api_key}`
+
+    urls = [["detail", url_detail], ["video", url_video],
+            ["credits", url_credits], ["reviews", url_reviews],
+            ["recommended", url_recommended]]
+
+    requests = []
+    urls.forEach((url) => {
+        req = buildReq(url[1])
+        requests.push(req)
+    })
     
-// }
+    //// PARSING FUNCTIONS
+    // DETAIL
+    function parseDetail(obj, entity) {
+        var result = {}
+        if (entity == "movie") {
+            result["name"] = obj.title
+            result["release_date"] = obj.release_date
+            result["runtime"] = obj.runtime
+        } 
+        else {
+            result["name"] = obj.name
+            result["first_air_date"] = obj.first_air_date
+            result["runtime"] = obj.episode_run_time[0]
+        }
+        if (obj.poster_path == null) {
+            result["poster_path"] = "https://cinemaone.net/images/movie_placeholder.png"
+        }
+        else {
+            result["poster_path"] = "https://image.tmdb.org/t/p/w500" + obj.poster_path
+        }
+
+        result["tmdb_id"] = obj.id
+        result["genres"] = obj.genres.map(item => item.name)
+        result["spoken_languages"] = obj.spoken_languages.map(item => item.english_name)
+        result["overview"] = obj.overview
+        result["vote_average"] = obj.vote_average
+        return result
+    }
+
+    // VIDEO
+    function parseVideo(obj, entity) {
+        var trailers = []
+        var teasers = []
+        obj.results.forEach((video) => {
+            if (video.site == "YouTube") {
+                if (video.type == "Trailer") {trailers.push(video)}
+                else if (video.type == "Teaser") {teasers.push(video)}
+            }
+        })
+
+        var result = {}
+        if (trailers.length > 0) {
+            result["site"] = trailers[0].site
+            result["type"] = trailers[0].type
+            result["name"] = trailers[0].name
+            result["key"] = trailers[0].key
+        }
+        else if (teasers.length > 0) {
+            result["site"] = trailers[0].site
+            result["type"] = trailers[0].type
+            result["name"] = trailers[0].name
+            result["key"] = trailers[0].key
+        }
+        return result
+    }
+    
+    // CREDITS
+    function parseCredits(obj) {
+        var result = []
+        obj.cast.forEach((person, i) => {
+            if (i < 10) { //Grab only the top 10 credits
+                var record = {}
+                record["id"] = person.id
+                record["name"] = person.name
+                record["character"] = person.character
+                if (person.profile_path == null) {
+                    record["profile_path"] = "https://bytes.usc.edu/cs571/s21_JSwasm00/hw/HW6/imgs/person-placeholder.png"
+                } else {
+                    record["profile_path"] = `https://image.tmdb.org/t/p/w500/${person.profile_path}`
+                }
+                result.push(record)
+            }
+        
+        })
+        return result
+    }
+
+    // REVIEWS
+    function parseReviews(obj) {
+        var result = []
+        obj.results.forEach((review, i) => {
+            if (i < 3) { //Grab only the top 3 reviews
+                var record = {}
+                record["author"] = review.author
+                record["content"] = review.content
+                record["created_at"] = review.created_at
+                record["url"] = review.url
+    
+                if (review.author_details.rating == null) {
+                    record["rating"] = 0
+                } else {
+                    record["rating"] = Math.round(review.author_details.rating)
+                }
+    
+                result.push(record)
+            }
+        })
+        return result
+    }
+
+
+    // RECOMMENDED
+    function parseRecommended(obj, entity) {
+        //Extract only necessary features from movie_obj
+        var result = []
+        if (obj.results.length == 0) {
+            return result
+        } else {
+            obj.results.forEach((item, i) => {
+                if (i < 20) { //Grab only the top 20
+                    var record = {}
+                    record["id"] = item.id
+        
+                    if (entity == "movie") {
+                        record["name"] = item.title
+                        record["entity_type"] = "movie"
+                    } else {
+                        record["name"] = item.name
+                        record["entity_type"] = "tv"
+                    }
+    
+                    if (item.poster_path == null) {
+                        record["poster_path"] = "https://cinemaone.net/images/movie_placeholder.png"
+                    } else {
+                        record["poster_path"] = "https://image.tmdb.org/t/p/w500" + item.poster_path
+                    }
+                    result.push(record)
+                }
+            })
+        }
+        return result
+    }
+
+    axios.all(requests).then(axios.spread((...responses) => {
+        //Parse desired features from each response,
+        // based on the response type (ex: "detail", "credits", "reviews")
+        var output = {}
+
+        //// DETAIL
+        obj = responses[0].data
+        detail = parseDetail(obj, entity)
+        output["detail"] = detail
+
+        //// VIDEO
+        obj = responses[1].data
+        video = parseVideo(obj, entity)
+        output["video"] = video
+
+        //// CREDITS
+        obj = responses[2].data
+        credits = parseCredits(obj)
+        output["credits"] = credits
+
+        //// REVIEWS
+        obj = responses[3].data
+        reviews = parseReviews(obj)
+        output["reviews"] = reviews
+
+        //// RECOMMENDED
+        obj = responses[4].data
+        recommended = parseRecommended(obj, entity)
+        output["recommended"] = recommended
+
+        // res.send("made it")
+        res.send(output)
+
+      })).catch(errors => {
+        // react on errors.
+      })
+
+})
+
 
 // Define port and listen on the port
 const PORT = process.env.PORT || 8080
